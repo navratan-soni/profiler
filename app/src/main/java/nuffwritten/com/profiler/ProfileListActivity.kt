dda45e72904fae40de62a7cacd0fec9b2f116236
Navratan <navratan@limeroad.com>
2018-10-15 00:02:42 +0530

    First commit

---
+++ app/src/main/java/nuffwritten/com/profiler/ProfileListActivity.kt
@@ -0,0 +1,12 @@
+package nuffwritten.com.profiler
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_list)
+    }
+}
