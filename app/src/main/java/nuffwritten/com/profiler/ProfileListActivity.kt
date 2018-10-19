e6aa9b85d1eeba298459c6354acb2fef2f7a307d
Navratan <navratan@limeroad.com>
2018-10-19 19:09:12 +0530

    Updated Gradle version kotlin version.

--- app/src/main/java/nuffwritten/com/profiler/ProfileListActivity.kt
+++ app/src/main/java/nuffwritten/com/profiler/ProfileListActivity.kt
@@ -2,11 +2,14 @@ package nuffwritten.com.profiler
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import nuffwritten.com.profiler.R.layout.activity_profile_list
 
 class ProfileListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile_list)
+        setContentView(activity_profile_list)
     }
+
+
 }
